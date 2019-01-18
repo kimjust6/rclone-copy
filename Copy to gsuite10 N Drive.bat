8dd5698b97fe9487585cc942f935775b5b1b12b7
MLT <kimjust6@gmail.com>
2019-01-18 14:32:11 -0500

    made the path to rclone.exe more generic.

--- Copy to gsuite10 N Drive.bat
+++ Copy to gsuite10 N Drive.bat
@@ -1,25 +1,25 @@
 TITLE Copy to gsuite11
-"D:\Desktop\Programs\rclone\rclone.exe" copy "N:\Videos\Videos" gsuite11:Videos -v --transfers 1  --ignore-case
-"D:\Desktop\Programs\rclone\rclone.exe" copy "N:\Videos\P" gsuite11:P -v --transfers 1  --ignore-case
+"%systemdrive%\Users\%USERNAME%\Desktop\Programs\rclone\rclone.exe" copy "N:\Videos\Videos" gsuite11:Videos -v --transfers 1  --ignore-case
+"%systemdrive%\Users\%USERNAME%\Desktop\Programs\rclone\rclone.exe" copy "N:\Videos\P" gsuite11:P -v --transfers 1  --ignore-case
 
 TITLE Copy to gsuite10
-"D:\Desktop\Programs\rclone\rclone.exe" copy "N:\Videos\Videos" gsuite10:Videos -v --transfers 1  --ignore-case
-"D:\Desktop\Programs\rclone\rclone.exe" copy "N:\Videos\P" gsuite10:P -v --transfers 1  --ignore-case
+"%systemdrive%\Users\%USERNAME%\Desktop\Programs\rclone\rclone.exe" copy "N:\Videos\Videos" gsuite10:Videos -v --transfers 1  --ignore-case
+"%systemdrive%\Users\%USERNAME%\Desktop\Programs\rclone\rclone.exe" copy "N:\Videos\P" gsuite10:P -v --transfers 1  --ignore-case
 
 TITLE Copy to csuite9
-"D:\Desktop\Programs\rclone\rclone.exe" copy "N:\Videos\Videos" csuite9:Videos -v --transfers 1  --ignore-case
-"D:\Desktop\Programs\rclone\rclone.exe" copy "N:\Videos\P" csuite9:P -v --transfers 1  --ignore-case
+"%systemdrive%\Users\%USERNAME%\Desktop\Programs\rclone\rclone.exe" copy "N:\Videos\Videos" csuite9:Videos -v --transfers 1  --ignore-case
+"%systemdrive%\Users\%USERNAME%\Desktop\Programs\rclone\rclone.exe" copy "N:\Videos\P" csuite9:P -v --transfers 1  --ignore-case
 
 TITLE Copy to csuite8
-"D:\Desktop\Programs\rclone\rclone.exe" copy "N:\Videos\Videos" csuite8:Videos -v --transfers 1  --ignore-case
-"D:\Desktop\Programs\rclone\rclone.exe" copy "N:\Videos\P" csuite8:P -v --transfers 1  --ignore-case
+"%systemdrive%\Users\%USERNAME%\Desktop\Programs\rclone\rclone.exe" copy "N:\Videos\Videos" csuite8:Videos -v --transfers 1  --ignore-case
+"%systemdrive%\Users\%USERNAME%\Desktop\Programs\rclone\rclone.exe" copy "N:\Videos\P" csuite8:P -v --transfers 1  --ignore-case
 
 TITLE Copy to csuite7
-"D:\Desktop\Programs\rclone\rclone.exe" copy "N:\Videos\Videos" csuite7:Videos -v --transfers 1  --ignore-case
-"D:\Desktop\Programs\rclone\rclone.exe" copy "N:\Videos\P" csuite7:P -v --transfers 1  --ignore-case
+"%systemdrive%\Users\%USERNAME%\Desktop\Programs\rclone\rclone.exe" copy "N:\Videos\Videos" csuite7:Videos -v --transfers 1  --ignore-case
+"%systemdrive%\Users\%USERNAME%\Desktop\Programs\rclone\rclone.exe" copy "N:\Videos\P" csuite7:P -v --transfers 1  --ignore-case
 
 TITLE Move to gsuite10
-"D:\Desktop\Programs\rclone\rclone.exe" move "N:\Videos\Videos" gsuite10:Videos -v --transfers 1  --ignore-case
-"D:\Desktop\Programs\rclone\rclone.exe" move "N:\Videos\P" gsuite10:P -v --transfers 1  --ignore-case
+"%systemdrive%\Users\%USERNAME%\Desktop\Programs\rclone\rclone.exe" move "N:\Videos\Videos" gsuite10:Videos -v --transfers 1  --ignore-case
+"%systemdrive%\Users\%USERNAME%\Desktop\Programs\rclone\rclone.exe" move "N:\Videos\P" gsuite10:P -v --transfers 1  --ignore-case
 
 Pause
