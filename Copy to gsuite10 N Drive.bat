1b5ee251bf993cdbf7ce9097366198ede0fe5b85
MLT <kimjust6@gmail.com>
2019-01-18 14:23:11 -0500

    Added the rclone copy batch script

---
+++ Copy to gsuite10 N Drive.bat
@@ -0,0 +1,21 @@
+TITLE Copy to gsuite10
+
+"D:\Desktop\Programs\rclone\rclone.exe" copy "N:\Videos\Videos" gsuite10:Videos -v --transfers 1  --ignore-case
+"D:\Desktop\Programs\rclone\rclone.exe" copy "N:\Videos\P" gsuite10:P -v --transfers 1  --ignore-case
+
+"D:\Desktop\Programs\rclone\rclone.exe" copy "N:\Videos\Videos" gsuite11:Videos -v --transfers 1  --ignore-case
+"D:\Desktop\Programs\rclone\rclone.exe" copy "N:\Videos\P" gsuite11:P -v --transfers 1  --ignore-case
+
+"D:\Desktop\Programs\rclone\rclone.exe" copy "N:\Videos\Videos" csuite9:Videos -v --transfers 1  --ignore-case
+"D:\Desktop\Programs\rclone\rclone.exe" copy "N:\Videos\P" csuite9:P -v --transfers 1  --ignore-case
+
+"D:\Desktop\Programs\rclone\rclone.exe" copy "N:\Videos\Videos" csuite8:Videos -v --transfers 1  --ignore-case
+"D:\Desktop\Programs\rclone\rclone.exe" copy "N:\Videos\P" csuite8:P -v --transfers 1  --ignore-case
+
+"D:\Desktop\Programs\rclone\rclone.exe" copy "N:\Videos\Videos" csuite7:Videos -v --transfers 1  --ignore-case
+"D:\Desktop\Programs\rclone\rclone.exe" copy "N:\Videos\P" csuite7:P -v --transfers 1  --ignore-case
+
+"D:\Desktop\Programs\rclone\rclone.exe" move "N:\Videos\Videos" gsuite10:Videos -v --transfers 1  --ignore-case
+"D:\Desktop\Programs\rclone\rclone.exe" move "N:\Videos\P" gsuite10:P -v --transfers 1  --ignore-case
+
+Pause
